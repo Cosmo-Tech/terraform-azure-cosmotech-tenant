08724884390d2e15c5881c29d67c3f9bc614a88d
Laurent Alépée <laurent.alepee@cosmotech.com>
2024-03-27 16:41:44 +0100

    fix to use tenant name to create storage account

--- azure-tenant-resources/main.tf
+++ azure-tenant-resources/main.tf
@@ -1,15 +1,14 @@
 locals {
-  cleaned_cluster_name        = replace(var.cluster_name, "/[[:^alnum:]]/", "")
-  cleaned_resource_group_name = replace(var.tenant_resource_group.name, "/[[:^alnum:]]/", "")
+  cleaned_tenant_name         = replace(var.tenant_resource_group.name, "/[[:^alnum:]]/", "")
   cleaned_managed_disk_name   = replace(var.managed_disk_name, "/[[:^alnum:]]/", "")
-  cosmosdb_name               = "csm${local.cleaned_cluster_name}-${local.cleaned_resource_group_name}"
-  eventhub_name               = substr("evname-${local.cleaned_cluster_name}-${local.cleaned_resource_group_name}", 0, 50)
-  kusto_name                  = substr("kusto${local.cleaned_resource_group_name}${random_string.random_storage_id.result}", 0, 21)
-  managed_disk_name           = local.cleaned_managed_disk_name != "" ? local.cleaned_managed_disk_name : substr("cosmotech-database-disk-${local.cleaned_resource_group_name}", 0, 80)
-  storage_name                = substr("${local.cleaned_cluster_name}${random_string.random_storage_id.result}", 0, 23)
-  container_registry_name     = substr("acr${local.cleaned_resource_group_name}${random_string.random_storage_id.result}", 0, 50)
-  backup_instance_name        = "cosmo-backup-instance-${local.cleaned_resource_group_name}"
-  backup_policy_name          = "cosmo-backup-policy-${local.cleaned_resource_group_name}"
+  cosmosdb_name               = "csm${local.cleaned_tenant_name}-${local.cleaned_tenant_name}"
+  eventhub_name               = substr("evname-${local.cleaned_tenant_name}-${local.cleaned_tenant_name}", 0, 50)
+  kusto_name                  = substr("kusto${local.cleaned_tenant_name}${random_string.random_storage_id.result}", 0, 21)
+  managed_disk_name           = local.cleaned_managed_disk_name != "" ? local.cleaned_managed_disk_name : substr("cosmotech-database-disk-${local.cleaned_tenant_name}", 0, 80)
+  storage_name                = substr("${local.cleaned_tenant_name}${random_string.random_storage_id.result}", 0, 23)
+  container_registry_name     = substr("acr${local.cleaned_tenant_name}${random_string.random_storage_id.result}", 0, 50)
+  backup_instance_name        = "cosmo-backup-instance-${local.cleaned_tenant_name}"
+  backup_policy_name          = "cosmo-backup-policy-${local.cleaned_tenant_name}"
   subnet_name                 = "default"
   tags                        = var.tags
 }
