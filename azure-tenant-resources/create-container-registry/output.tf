cf611298ac9c20095c7a1d8fffdf0dafb744626b
nibaldo <nibaldo.donoso@cosmotech.com>
2024-07-31 12:10:22 +0200

    add outputs for kubernetes deploy secrets

--- azure-tenant-resources/create-container-registry/output.tf
+++ azure-tenant-resources/create-container-registry/output.tf
@@ -1,6 +1,5 @@
 output "out_acr_login_server" {
   value     = azurerm_container_registry.acr.login_server
-  sensitive = true
 }
 
 output "out_acr_login_username" {
@@ -11,4 +10,8 @@ output "out_acr_login_username" {
 output "out_acr_login_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
+}
+
+output "out_acr_login_server_url" {
+  value     = "https://${azurerm_container_registry.acr.login_server}"
 }
