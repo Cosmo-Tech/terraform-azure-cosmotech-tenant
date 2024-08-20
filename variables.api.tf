2a91ee81f8011d92cb09886250fa512baa548650
nibaldo <nibaldo.donoso@cosmotech.com>
2024-08-20 11:31:34 +0200

    fix fmt and validate

---
+++ variables.api.tf
@@ -0,0 +1,15 @@
+variable "identity_authorization_url" {
+  type = string
+}
+variable "identity_token_url" {
+  type = string
+}
+variable "deploy_api" {
+  type = bool
+}
+variable "create_platform_config" {
+  type = bool
+}
+variable "cosmotech_api_chart_package_version" {
+  type = string
+}
