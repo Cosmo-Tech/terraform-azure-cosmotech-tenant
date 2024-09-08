090451954e43734134cf7d0250d19b4759ec55e5
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-08 16:35:10 +0200

    refacto acr policy with standard sku [skip ci]

--- variables.api.tf
+++ variables.api.tf
@@ -5,13 +5,19 @@ variable "create_platform_config" {
 variable "cosmotech_api_chart_package_version" {
   type        = string
   description = "The version of the Cosmo Tech API chart to deploy"
-  default     = "3.1.2"
+  validation {
+    condition     = var.cosmotech_api_chart_package_version == var.cosmotech_api_version
+    error_message = "The chart version must match with api version"
+  }
 }
 
 variable "cosmotech_api_version_path" {
   description = "The API version path"
   type        = string
-  default     = "v3-1"
+  validation {
+    condition     = "v${split(".", var.cosmotech_api_version)[0]}-${split(".", var.cosmotech_api_version)[1]}" == var.cosmotech_api_version_path
+    error_message = "The version of cosmotech api do not match with version path"
+  }
 }
 
 variable "cosmotech_api_version" {
