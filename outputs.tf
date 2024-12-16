eafd49efe330a56d1ce8ad65ed9179f423ff43c5
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-16 13:38:59 +0100

    fix fmt

--- outputs.tf
+++ outputs.tf
@@ -94,7 +94,7 @@ output "out_restish_sp_client_id" {
 }
 
 output "out_restish_sp_client_secret" {
-  value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_restish_sp_client_secret : var.restish_sp_client_secret
+  value     = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_restish_sp_client_secret : var.restish_sp_client_secret
   sensitive = true
 }
 
