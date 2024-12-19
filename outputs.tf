c6f8f0e418d5833ba545c0dd18d446ea73510ced
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-19 14:24:45 +0100

    fix outputs for backend entries

--- outputs.tf
+++ outputs.tf
@@ -15,27 +15,27 @@ output "out_acr_login_password" {
   sensitive = true
 }
 
-output "out_adx_uri" {
-  value = module.azure-tenant-resources.out_adx_uri
+output "out_adx_cluster_uri" {
+  value = module.azure-tenant-resources.out_adx_cluster_uri
 }
 
-output "out_cluster_adx_principal_id" {
-  value = module.azure-tenant-resources.out_adx_principal_id
+output "out_adx_cluster_principal_id" {
+  value = module.azure-tenant-resources.out_adx_cluster_principal_id
 }
 
-output "out_cluster_adx_name" {
-  value = module.azure-tenant-resources.out_adx_name
+output "out_adx_cluster_name" {
+  value = module.azure-tenant-resources.out_adx_cluster_name
 }
 
-output "out_cosmo_api_url" {
+output "out_api_cosmo_url" {
   value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_cosmo_api_url : "https://${var.network_dns_record}.${var.network_dns_zone_name}/${var.kubernetes_tenant_namespace}/${var.cosmotech_api_version_path}"
 }
 
-output "out_cosmo_api_scope" {
+output "out_api_cosmo_scope" {
   value = "https://${var.network_dns_record}.${var.network_dns_zone_name}/${var.kubernetes_tenant_namespace}/.default"
 }
 
-output "out_cosmo_api_version_path" {
+output "out_api_cosmo_version_path" {
   value = var.cosmotech_api_version_path
 }
 
@@ -43,16 +43,24 @@ output "out_tenant_resource_group" {
   value = var.tenant_resource_group
 }
 
-output "out_location" {
+output "out_tenant_sp_client_id" {
+  value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_platform_sp_client_id : var.client_id
+}
+
+output "out_tenant_sp_object_id" {
+  value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_platform_sp_object_id : var.tenant_sp_object_id
+}
+
+output "out_azure_resource_location" {
   value = var.location
 }
 
-output "out_storage_account_name" {
-  value = module.azure-tenant-resources.out_storage_account_name
+output "out_azure_storage_account_name" {
+  value = module.azure-tenant-resources.out_azure_storage_account_name
 }
 
-output "out_storage_account_key" {
-  value     = module.azure-tenant-resources.out_storage_account_key
+output "out_azure_storage_account_key" {
+  value     = module.azure-tenant-resources.out_azure_storage_account_key
   sensitive = true
 }
 
@@ -67,14 +75,6 @@ output "out_babylon_sp_object_id" {
 output "out_babylon_sp_client_secret" {
   value     = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_babylon_sp_client_secret : var.babylon_sp_client_secret
   sensitive = true
-}
-
-output "out_tenant_sp_client_id" {
-  value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_platform_sp_client_id : var.client_id
-}
-
-output "out_tenant_sp_object_id" {
-  value = var.deployment_type != "ARM" ? module.azure-tenant-prerequisites.0.out_platform_sp_object_id : var.tenant_sp_object_id
 }
 
 output "out_subscription_id" {
