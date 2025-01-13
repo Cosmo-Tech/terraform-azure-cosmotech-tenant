2d466c5420b9770b91a7c61ae921804b06a24860
nibaldo <nibaldo.donoso@cosmotech.com>
2025-01-13 13:38:42 +0100

    add variable admin activate k8s

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 locals {
-  kube_config     = data.azurerm_kubernetes_cluster.current.kube_admin_config
+  kube_config     = var.kubernetes_cluster_admin_activate ? data.azurerm_kubernetes_cluster.current.kube_admin_config : data.azurerm_kubernetes_cluster.current.kube_config
   tls_secret_name = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
 }
 
