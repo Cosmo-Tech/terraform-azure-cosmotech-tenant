6781f939c9f7aed8bbc9d7e4353710ed903b67d7
nibaldo <nibaldo.donoso@cosmotech.com>
2025-01-13 11:50:30 +0100

    add ourputs for summary report

--- azure-tenant-prerequisites/outputs.babylon.tf
+++ azure-tenant-prerequisites/outputs.babylon.tf
@@ -10,3 +10,7 @@ output "out_babylon_sp_client_secret" {
   value     = var.create_babylon ? azuread_application_password.babylon_password.0.value : null
   sensitive = true
 }
+
+output "out_babylon_sp_name" {
+  value = var.create_babylon ? azuread_service_principal.babylon.0.display_name : null
+}
