7f709c5fd60264b38a9be5c3d8de524f992cc444
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-03 13:02:47 +0100

    add postgres persistence

---
+++ variables.pv-postgres.tf
@@ -0,0 +1,18 @@
+variable "pv_postgres_deploy" {
+  type = bool
+}
+variable "pv_postgres_storage_gbi" {
+  type = number
+}
+variable "pv_postgres_storage_account_type" {
+  type = string
+}
+variable "pv_postgres_replicas" {
+  type = number
+}
+variable "pv_postgres_storage_class_name" {
+  type = string
+}
+variable "pv_postgres_provider" {
+  type = string
+}
