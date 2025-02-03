94ffa2be70a895656e70da14fc3c9ed052d50a4c
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-03 13:18:19 +0100

    add seaweedfs persistence

---
+++ variables.pv-seaweedfs.tf
@@ -0,0 +1,18 @@
+variable "pv_seaweedfs_deploy" {
+  type = bool
+}
+variable "pv_seaweedfs_storage_gbi" {
+  type = number
+}
+variable "pv_seaweedfs_storage_account_type" {
+  type = string
+}
+variable "pv_seaweedfs_replicas" {
+  type = number
+}
+variable "pv_seaweedfs_storage_class_name" {
+  type = string
+}
+variable "pv_seaweedfs_provider" {
+  type = string
+}
