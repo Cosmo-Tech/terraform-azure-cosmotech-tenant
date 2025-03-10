40bd1ea9b4d5aa957b6d41dc949eac61a78fe3bc
nibaldo <nibaldo.donoso@cosmotech.com>
2025-03-10 09:59:01 +0100

    add vars to setup providers [skip ci]

--- providers.tf
+++ providers.tf
@@ -42,3 +42,27 @@ provider "azuread" {
   client_id     = var.client_id
   client_secret = var.client_secret
 }
+
+provider "kubernetes" {
+  host                   = var.host
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.host
+    client_certificate     = var.client_certificate
+    client_key             = var.client_key
+    cluster_ca_certificate = var.cluster_ca_certificate
+  }
+}
+
+provider "kubectl" {
+  host                   = var.host
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+  load_config_file       = false
+}
