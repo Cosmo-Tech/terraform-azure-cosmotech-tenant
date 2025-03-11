6e42192a72af8c8d01efa7a6390f205858375a9c
nibaldo <nibaldo.donoso@cosmotech.com>
2025-03-11 17:01:04 +0100

    ficsqxsq outputs [skip ci]

--- create-kusto/output.tf
+++ create-kusto/output.tf
@@ -1,15 +1,15 @@
-output "out_adx_name" {
+output "out_adx_cluster_name" {
   value = azurerm_kusto_cluster.kusto.name
 }
 
-output "out_adx_uri" {
+output "out_adx_cluster_uri" {
   value = azurerm_kusto_cluster.kusto.uri
 }
 
-output "out_adx_ingestion_uri" {
+output "out_adx_cluster_ingestion_uri" {
   value = azurerm_kusto_cluster.kusto.data_ingestion_uri
 }
 
-output "out_adx_principal_id" {
+output "out_adx_cluster_principal_id" {
   value = azurerm_kusto_cluster.kusto.identity.0.principal_id
 }
--- variables.backend-remote.tf
+++ variables.backend-remote.tf
@@ -1,20 +1,15 @@
-# Backend remote vars
 variable "tf_resource_group_name" {
   type = string
 }
-
 variable "tf_storage_account_name" {
   type = string
 }
-
 variable "tf_container_name" {
   type = string
 }
-
 variable "tf_blob_name_tenant" {
   type = string
 }
-
 variable "tf_access_key" {
   type = string
 }
