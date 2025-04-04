d9ec750bfc8b352a8c83b79d94d8d6c2cbf28d5c
nibaldo <nibaldo.donoso@cosmotech.com>
2025-04-04 16:29:41 +0200

    fix azurerm definition

--- providers.tf
+++ providers.tf
@@ -27,3 +27,15 @@ terraform {
 provider "kubernetes" {
   config_path = var.config_path
 }
+
+terraform {
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
